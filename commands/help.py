c070d5178c352e72b1675f53d5addc53eacf3848
tlukanie <tanyanewem@gmail.com>
2025-05-31 16:15:37 +0200

    removed

--- commands/help.py
+++
@@ -1,6 +0,0 @@
-def cmd_help():
-    print("Commands:")
-    print("  identify [hash]  - lookup Sepolia tx")
-    print("  wallet [address] - get information about an Ethereum wallet")
-    print("  help             - this menu")
-    print("  exit             - quit")
